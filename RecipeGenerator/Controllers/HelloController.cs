6686d57519fb262432a8b9c0fb8ce91883f3313e
Emmanuell <esevieux@gmail.com>
2023-08-21 18:13:49 -0700

    Eman's Recipe Generator (Broken Stream Reader version)

---
+++ RecipeGenerator/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("HwlloWorld");
+        }
+    }
+}
---
+++ RecipeGenerator/Controllers/setIngredientController.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+//using System.Runtime.InteropServices.JavaScript;
+
+namespace RecipeGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class setIngredientController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hello world");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            var response = "Here's your list of ingredients: " + payload;
+            return Ok(response);
+        }
+    }
+}
