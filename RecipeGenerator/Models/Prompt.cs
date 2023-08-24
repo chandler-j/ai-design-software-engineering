76633f942ab0fb74918a239499eee62b65128f0d
DansoVA <veradansoh@gmail.com>
2023-08-24 19:11:47 -0400

    2.1 Bug : Fixed missed aligned image
    Cleaned up presentation of images fixed image border; Added redirect for recipes saved.

--- RecipeGenerator/Models/Prompt.cs
+++ RecipeGenerator/Models/Prompt.cs
@@ -3,7 +3,7 @@ namespace RecipeGenerator.Models
 {
 	public class Prompt
 	{
-        public string Ingredients { get; set; }
+        public string? Ingredients { get; set; }
         public string? CuisineType { get; set; } // ? meanse it's nullable
     }
 }
